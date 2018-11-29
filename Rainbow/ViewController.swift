3cc1c4f626270540361f3b27918a0c2520eaade7
Shawn <Shawn@shawns-macbook-pro.local>
2018-11-29 11:25:08 +0800

    Initial Commit

---
+++ Rainbow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Shawn on 29/11/2018.
+//  Copyright © 2018 Shawn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
